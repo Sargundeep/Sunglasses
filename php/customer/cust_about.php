57391f303fc93423a10cebf28dd8b6e6ce6647a9
Sargundeep <sargundeepkaur.s@somaiya.edu>
2022-12-04 23:12:27 +0530

    refract: about page

--- php/customer/cust_about.php
+++ php/customer/cust_about.php
@@ -43,26 +43,9 @@
       <?php 
          include("../includes/navbar.php");
       ?> 
-      <!-- about section -->
-      <div class="about">
-         <div class="container">
-            <div class="row d_flex">
-               <div class="col-md-5">
-                  <div class="about_img">
-                     <figure><img src="../../images/about_img.png" alt="#"/></figure>
-                  </div>
-               </div>
-               <div class="col-md-7">
-                  <div class="titlepage">
-                     <h2>About Our Shop</h2>
-                     <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.</p>
-                  </div>
-                  <a class="read_more" href="#">Read More</a>
-               </div>
-            </div>
-         </div>
-      </div>
-      <!-- about section -->
+      <?php 
+         include("../includes/about.php");
+      ?>
       <?php 
          include("../includes/footer.php");
       ?> 
