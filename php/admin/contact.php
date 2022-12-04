b3cceb514e0261f2e9f446b2bb94178af045e545
Hussain Pettiwala <hussain.pettiwala@gmail.com>
2022-12-04 18:56:42 +0530

    feat: disable access to admin screens without admin username

--- php/admin/contact.php
+++ php/admin/contact.php
@@ -1,3 +1,9 @@
+<?php
+if($_SESSION['username']  != "Admin") {
+   header("location: ../signupform.php");
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
    <head>
--- php/admin/form.php
+++ php/admin/form.php
@@ -1,3 +1,10 @@
+<?php
+if($_SESSION['username']  != "Admin") {
+   header("location: ../signupform.php");
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
    <head>
