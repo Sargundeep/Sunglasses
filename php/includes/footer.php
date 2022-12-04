d705fdfd153311ceea7d64530f107ccd7f015e18
Sargundeep <sargundeepkaur.s@somaiya.edu>
2022-12-04 23:18:09 +0530

    refract: contact page css corrected

--- php/includes/footer.php
+++ php/includes/footer.php
@@ -5,9 +5,9 @@
                <div class="row">
                   <div class="col-md-8 offset-md-2">
                      <ul class="location_icon">
-                        <li><a href="#"><i class="fa fa-map-marker" aria-hidden="true"></i></a><br> Location</li>
+                        <li><a href="#"><i class="fa fa-map-marker" aria-hidden="true"></i></a><br>KJSCE</li>
                         <li><a href="#"><i class="fa fa-phone" aria-hidden="true"></i></a><br> +01 1234567890</li>
-                        <li><a href="#"><i class="fa fa-envelope" aria-hidden="true"></i></a><br> demo@gmail.com</li>
+                        <li><a href="#"><i class="fa fa-envelope" aria-hidden="true"></i></a><br>sunglahelpdesk@gmail.com</li>
                      </ul>
                   </div>
                </div>
