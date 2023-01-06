2366714b6668e64779395323e99661bf6e548537
Sargundeep <sargundeepkaur.s@somaiya.edu>
2023-01-06 19:47:00 +0530

    refract:cleaned

--- inc/config.php
+++ inc/config.php
@@ -4,14 +4,14 @@ define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'root');
 define('DB_NAME', 'sungla');
- 
+
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
 $DB_SERVER = 'mysql:dbname=sungla;host=localhost';
 $DB_USERNAME = 'root';
 $DB_PASSWORD = 'abc123';
- 
+
 try
 {
 	$link = new PDO($DB_SERVER,$DB_USERNAME,$DB_PASSWORD);
@@ -22,7 +22,7 @@ catch(PDOException $error)
 	echo "PDO error".$error->getMessage();
 	die();
 }
- 
+
 define('PRODUCT_IMG_URL','images/product-images/');
- 
+
 ?>
